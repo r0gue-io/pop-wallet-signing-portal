07f620780157a0008f94c854812e001354b50436
Peter White <petras9789@gmail.com>
2024-12-11 19:45:27 -0700

    feat: single file output

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended'
+  ],
+  rules: {
+    'quotes': ['error', 'single']
+  }
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import { viteSingleFile } from "vite-plugin-singlefile"
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), viteSingleFile()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
