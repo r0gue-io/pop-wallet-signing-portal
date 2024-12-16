17ec5c7147dfdbe22155cea27a2dcdd0842b0fdf
Peter White <petras9789@gmail.com>
2024-12-16 13:15:01 -0700

    chore: cleanup boilerplate

--- src/api/clients/dot.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from "polkadot-api"
-import { getSmProvider } from "polkadot-api/sm-provider"
-import { smoldot } from "./smoldot"
-
-export const dotRelayChain = import("polkadot-api/chains/polkadot").then(
-  ({ chainSpec }) => smoldot.addChain({ chainSpec }),
-)
-
-export const dotClient = createClient(getSmProvider(dotRelayChain))
