90facc9fe7d9f81da73d803be13da715e2203938
Peter White <petras9789@gmail.com>
2024-12-16 13:35:37 -0700

    chore: remove comments from output

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,13 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      output: {
+        // Remove all comments
+        comments: false,
+      },
+    },
+  },
 })
