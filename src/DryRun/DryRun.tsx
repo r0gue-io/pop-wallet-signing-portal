b728984268601a61964cc20ef9c46362e43ae324
AlexD10S <alexfraga10@gmail.com>
2025-03-29 15:48:42 +0100

    fix: message to map account

--- src/DryRun/DryRun.tsx
+++ src/DryRun/DryRun.tsx
@@ -209,7 +209,7 @@ const ContractExecution: React.FC<{ result: ContractExecutionResult, originalGas
             {result.result?.value?.value?.value?.type === "AccountUnmapped" && (
               <div>
                 <span>Account not mapped. </span>
-                <span className="mt-1">Run this command to fix:</span>
+                <span className="mt-1">You can map it by running the following command:</span>
                 <pre className="bg-red-50 text-red-800 text-sm p-3 mt-2 rounded-md border border-red-200 whitespace-pre-wrap break-words">
                   pop call chain --pallet Revive --function map_account --url {rpc} --use-wallet
                 </pre>
